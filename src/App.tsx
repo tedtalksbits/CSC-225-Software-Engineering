185838471eb1020feca6aec85023614797a2dff7
Isaiah <Isaiahbrodriguez13@gmail.com>
2024-03-04 18:07:00 -0500

    feat: Added Date

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import { useState } from 'react';
-import './App.css';
-import { Button } from '@/components/ui/button';
+import { useState } from "react";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import { time } from "console";
 
 function App() {
-  const [theme, setTheme] = useState<'light' | 'dark' | 'system'>('system');
+  const [theme, setTheme] = useState<"light" | "dark" | "system">("system");
   async function handleToggleDarkTheme() {
     const isDarkMode = await window.electron.theme.toggle();
-    setTheme(isDarkMode ? 'dark' : 'light');
+    setTheme(isDarkMode ? "dark" : "light");
   }
   const handleClick = () => {
-    alert('You clicked the button');
+    alert("You clicked the button");
   };
 
   return (
-    <div className='h-screen w-screen grid place-content-center gap-4'>
+    <div className="h-screen w-screen grid place-content-center gap-4">
       <Button onClick={handleToggleDarkTheme}>
-        Switch to {theme === 'dark' ? '🌞' : '🌙'} Mode
+        Switch to {theme === "dark" ? "🌞" : "🌙"} Mode
       </Button>
-      <div className='container border p-8 rounded-md space-y-8'>
-        <h1 className='text-center text-3xl font-bold '>Let's go boiz</h1>
+      <div className="container border p-8 rounded-md space-y-8">
+        <h1>
+          Todays Date: <Date></Date>
+        </h1>
+        <h1 className="text-center text-3xl font-bold ">Let's go boiz</h1>
         <Button onClick={handleClick}>Click the button for a suprise</Button>
       </div>
     </div>
