d6c389c4616b5020a1c4431144b8856436e01b4a
Isaiah <Isaiahbrodriguez13@gmail.com>
2024-03-04 18:24:57 -0500

    Chore: Date-spacing in CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import { Button } from "@/components/ui/button";
-import { time } from "console";
 
 function App() {
   const [theme, setTheme] = useState<"light" | "dark" | "system">("system");
@@ -12,15 +11,18 @@ function App() {
   const handleClick = () => {
     alert("You clicked the button");
   };
+  
+    const currentDate = new Date();
+    const formattedDate = `${currentDate.getMonth() + 1} / ${currentDate.getDate()} / ${currentDate.getFullYear()}`;
 
   return (
     <div className="h-screen w-screen grid place-content-center gap-4">
       <Button onClick={handleToggleDarkTheme}>
         Switch to {theme === "dark" ? "🌞" : "🌙"} Mode
       </Button>
+          <div className="todays__Date">Todays Date: {formattedDate}</div>
       <div className="container border p-8 rounded-md space-y-8">
         <h1>
-          Todays Date: <Date></Date>
         </h1>
         <h1 className="text-center text-3xl font-bold ">Let's go boiz</h1>
         <Button onClick={handleClick}>Click the button for a suprise</Button>
